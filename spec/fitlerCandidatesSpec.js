3833bec9c93311ad02a1458ac8eb9b83ffc76a64
Caitlin Cooling <ccooling97@gmail.com>
2019-01-03 10:00:07 +0000

    Test: filterCandidateBySkill - When Skill is JavaScript, return array of candidate hashes that have JavaScript skill

---
+++ spec/fitlerCandidatesSpec.js
@@ -0,0 +1,22 @@
+describe('FilterCandidates', function() {
+  const newCandidates = [
+    { name: "Kerrie", skills: ["JavaScript", "Docker", "Ruby"] },
+    { name: "Mario", skills: ["Python", "AWS"] },
+    { name: "Jacquline", skills: ["JavaScript", "Azure"] },
+    { name: "Kathy", skills: ["JavaScript", "Java"] },
+    { name: "Anna", skills: ["JavaScript", "AWS"] },
+    { name: "Matt", skills: ["PHP", "AWS"] },
+    { name: "Matt", skills: ["PHP", ".Net", "Docker"] },
+  ];
+
+  it('whose skill includes JavaScript', function() {
+    expect(filterCandidateBySkill(newCandidates, "JavaScript")).toEqual([
+      { name: "Kerrie", skills: ["JavaScript", "Docker", "Ruby"] },
+      { name: "Jacquline", skills: ["JavaScript", "Azure"] },
+      { name: "Kathy", skills: ["JavaScript", "Java"] },
+      { name: "Anna", skills: ["JavaScript", "AWS"] },
+    ]);
+  });
+
+
+});
